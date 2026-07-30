95246a72a23d28a3696fe7cc004405c936ce7bf2
dh1-dev <daniel.hollingworth@hotmail.co.uk>
2026-07-30 16:26:20 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,11 @@
-self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())await caches.delete(k);await self.registration.unregister();for(const c of await self.clients.matchAll({type:'window'}))c.navigate(c.url)})()));
+self.addEventListener('install', event => self.skipWaiting());
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const names = await caches.keys();
+    await Promise.all(names.map(name => caches.delete(name)));
+    await self.registration.unregister();
+    const clients = await self.clients.matchAll({ type: 'window' });
+    clients.forEach(client => client.navigate(client.url));
+  })());
+});
+self.addEventListener('fetch', () => {});
