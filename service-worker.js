aeb36b5186220c2583fdcaa97b50ff8ef8ea6ed9
dh1-dev <daniel.hollingworth@hotmail.co.uk>
2026-07-30 08:47:18 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='organiseus-v20260730-original-auth-restored';
-const FILES=['./','./index.html','./manifest.webmanifest?v=authrestore1','./icon-full-192.png?v=authrestore1','./icon-full-512.png?v=authrestore1'];
+const CACHE='organiseus-v20260730-explicit-session-v3';
+const FILES=['./','./index.html','./manifest.webmanifest?v=sessionv3','./icon-full-192.png?v=sessionv3','./icon-full-512.png?v=sessionv3'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
