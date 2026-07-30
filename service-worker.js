a5b6e61804ee0864e6444689fd133547efd5ff2a
dh1-dev <daniel.hollingworth@hotmail.co.uk>
2026-07-30 07:05:01 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='organiseus-recovery-core-first-4';
-const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='organiseus-refresh-session-fullbleed-v4';
+const FILES=['./','./index.html','./manifest.webmanifest?v=fullbleed4','./icon-full-192.png?v=4','./icon-full-512.png?v=4'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
