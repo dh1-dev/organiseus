cd9c9fa535573309647b95924acb6d6ee695f103
dh1-dev <daniel.hollingworth@hotmail.co.uk>
2026-07-30 16:10:03 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,4 @@
-const CACHE='organiseus-v20260730-explicit-session-v3';
-const FILES=['./','./index.html','./manifest.webmanifest?v=sessionv3','./icon-full-192.png?v=sessionv3','./icon-full-512.png?v=sessionv3'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET')return;
-  event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{
-    const copy=response.clone();
-    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-    return response;
-  }).catch(()=>caches.match(event.request)));
-});
+const CACHE_NAME='organiseus-remove-old-caches-v1';
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));await self.registration.unregister();const clientsList=await self.clients.matchAll({type:'window'});for(const client of clientsList)client.navigate(client.url);})()));
+self.addEventListener('fetch',()=>{});
